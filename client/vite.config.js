d6ba206333e910456c3dea95cdff089722211cae
Keerthan <keerthanm1373@gmail.com>
2024-08-08 12:42:28 +0530

    new era come

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
@@ -19,6 +17,11 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       'process.env': process.env
+    },
+    build: {
+      rollupOptions: {
+        external: [] // Add your external dependencies here if any
+      }
     }
   };
 });
